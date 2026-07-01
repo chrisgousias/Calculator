a7a215ab8b26aef05ee2d83b2550dddc8ce40fce
chrisgousias <182855761+chrisgousias@users.noreply.github.com>
2026-07-01 16:35:50 +0300

    Created functions: add,subtract,multiply,divide and three variables for the operation

---
+++ javascript.js
@@ -0,0 +1,34 @@
+/* 
+Your calculator is going to contain functions for all 
+of the basic math operators you typically find on 
+calculators, so start by creating functions for the 
+following items and testing them in your 
+browser’s console:
+    add
+    subtract
+    multiply
+    divide
+*/
+
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+/* 
+2nd step:
+*/
+const firstNumber;
+const operator;
+const secondNumber;
